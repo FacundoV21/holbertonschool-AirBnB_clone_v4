1c3200f221bc02a13381d9e6cbf694ff5f5b7bb3
FacundoV21 <6756@holbertonstudents.com>
2024-02-10 12:29:20 -0800

    feat: added task 3, fixed: format errors

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -4,6 +4,16 @@ window.onload = () => {
     if ($(this).is(":checked")) {
       list[$(this).attr("data-id")] = $(this).attr("data-name")
     } else { delete list[$(this).attr("data-id")] }
+
     $(" .amenities h4").text(Object.values(list).join(", "))
+  })
+  $.ajax({
+    url: 'http://localhost:5001/api/v1/status',
+    method: 'GET',
+    success: function(data) {
+      if (data.status === 'OK') {
+        $("div#api_status").addClass('available')
+      } else { $("div#api_status").classList.remove('available') }
+    }
   })
 }
